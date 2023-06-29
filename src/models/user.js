b1d28029f94ada49cf17ddef7d9f02dcd5df8cfb
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-29 17:43:00 +0400

    update staff seekers

--- src/models/user.js
+++ src/models/user.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   }
   User.init({
@@ -23,3 +17,4 @@ module.exports = (sequelize, DataTypes) => {
   });
   return User;
 };
+

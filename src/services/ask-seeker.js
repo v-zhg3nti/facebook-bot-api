187adc2ad1633df80ff50aa639c23aadbcca40b8
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-07-20 20:35:05 +0400

    🚀 workin code jaba

--- src/services/ask-seeker.js
+++ src/services/ask-seeker.js
@@ -1,13 +1,11 @@
 
 require("dotenv").config("../../.env");
-
 const axiosInstance = require("../http/index");
 const {
   facebook: { access_token },
 } = require("../config/index");
 
-// const { createPayload } = require("../utils/index");
-// const { handler1Payload } = require("../data/index");
+
 const {
   updateSession,
   getSession,
@@ -54,14 +52,10 @@ async function handler1(sessionId) {
                 },
                 {
                   type: "postback",
-                  title: "პირობები დამქირავებლის",
+                  title: "პირობები",
                   payload: "PIROBEBI",
                 },
-                {
-                  type: "postback",
-                  title: "პირობები კანდიდატის",
-                  payload: "PIROBEBI2",
-                },
+
               ],
             },
           ],
@@ -111,9 +105,8 @@ async function handler2(sessionId, messaging) {
       
       სადაც თქვენთვის უფრო ხელსაყრელი იქნება. სამუშაო საათებია: ორშაბათიდან შაბათის ჩათვლით დილის 10:00-დან საღამოს 18:00-მდე, ხოლო თუ მხოლოდ კვირა დღეს გცალიათ და გაწყობთ მობრძანება, მაშინ რუსთაველზე, არჩილ ჯორჯაძის ქ.№5 (ქაშუეთის ეკლესიის გვერდით)
          https://goo.gl/maps/VnYrzPZeATt
-         მდებარე ფილიალში შეგიძლიათ მობრძანდეთ.
-      
-      პატივისცემით: თბილისის საოჯახო პერსონალის საკადრო ცენტრ ,,ბაია"-ს ადმინისტრაცია.`;
+         მდებარე ფილიალში შეგიძლიათ მობრძანდეთ.`;
+
       const payload = {
         messaging_type: "RESPONSE",
         recipient: {
@@ -121,8 +114,96 @@ async function handler2(sessionId, messaging) {
         },
         message: {
           text: contactInfo,
+
         },
       };
+            // await deleteSession(sessionId);
+
+
+      const request = await axiosInstance();
+      // await deleteSession(sessionId);
+
+      const response = await request.post(
+        `/me/messages?access_token=${access_token}`,
+        payload
+      );
+
+      return response;
+    } else if (answer === "PIROBEBI") {
+      const aboutInfo = `👇 გამარჯობა, გთხოვთ გაეცნოთ ჩვენს პირობებს 👇
+
+      🌹კანდიდატის პირობები🌹 
+      
+      ჩვენ გაგასაუბრებთ თქვენს მოთხოვნებთან მაქსიმალურ შესაბამისობაში 4-5 პასპორტიზირებულ კანდიდატს, ყველასათვის ცნობებით და რომელიმე მოწონებისა და გამოსაცდელი ვადის წარმატებით გავლის შემთხვევაში ჩვენთან იხდით კადრის მოძიების საფასურს, რომელიც განისაზღვრება თვიურ ხელფასს მხოლოდ 10%-ს ერთჯერადად. რაც შედის 1 წლის განმავლობაში, კადრი, თუ წავიდა სამსახურიდან, მისგან დამოუკიდებელი მიზეზების გამო ან თავად დაითხოვეთ, უფასოდ გაგინაცვლებთ 3 ჯერ სხვა კანდიდატურით.
+      
+
+      🌹პერსონალის პირობები🌹 
+      
+      ჩვენი პირობები შემდგომში მდგომარეობს: პირველ თვეს ანაზღაურებიდან ნახევარი ჩამოგეჭრებათ მას შემდეგ, რაც აიღებთ ამ ანაზღაურებას. რეგისტრაცია კი ფასიანია და ავანსად გადახდილი იქნება 30 ლარი.
+      
+      `;
+
+      const payload = {
+        messaging_type: "RESPONSE",
+        recipient: {
+          id: sessionId,
+        },
+        message: {
+          text: aboutInfo,
+
+        },
+      };
+      const request = await axiosInstance();
+      // await deleteSession(sessionId);
+
+      const response = await request.post(
+        `/me/messages?access_token=${access_token}`,
+        payload
+      );
+
+      return response;
+    }
+  } catch (error) {
+    console.log("Error acquired in handler 2:", error);
+    throw error;
+  }
+}
+
+async function handler3(sessionId, messaging) {
+  const answer = messaging[0]?.postback?.payload;
+  try {
+    let session = await getSession(sessionId);
+
+    if (answer === "CONTACT") {
+      const contactInfo = `Contact Information:
+      🌹 1) არჩილ ჯორჯაძის ქ.№5 (ქაშუეთის ეკლესიის გვერდით)
+         https://goo.gl/maps/VnYrzPZeATt
+         T: {{511110225}}
+      
+      🌺 2) ვაჟა ფშაველას გამზ N76ბ (მ/სადგურ ,,ვაჟა ფშაველა"-ს გვერდით, მარჯვენა ამოსასვლელთან მდებარე კორპუსი)
+         https://goo.gl/maps/fZjatKHFqWQ2
+         T: {{599613090}}
+      
+      🌲 3) ილია ჭავჭავაძის გამზ N2 (ვაკე - თ.ს.უ-ს I-კორპუსის წინ)
+         https://goo.gl/maps/VnCSaWhpwNPoVXcJ6
+         T: {{599191507}}
+      
+      სადაც თქვენთვის უფრო ხელსაყრელი იქნება. სამუშაო საათებია: ორშაბათიდან შაბათის ჩათვლით დილის 10:00-დან საღამოს 18:00-მდე, ხოლო თუ მხოლოდ კვირა დღეს გცალიათ და გაწყობთ მობრძანება, მაშინ რუსთაველზე, არჩილ ჯორჯაძის ქ.№5 (ქაშუეთის ეკლესიის გვერდით)
+         https://goo.gl/maps/VnYrzPZeATt
+         მდებარე ფილიალში შეგიძლიათ მობრძანდეთ.`;
+
+      const payload = {
+        messaging_type: "RESPONSE",
+        recipient: {
+          id: sessionId,
+        },
+        message: {
+          text: contactInfo,
+
+        },
+      };
+            // await deleteSession(sessionId);
+
 
       const request = await axiosInstance();
       await deleteSession(sessionId);
@@ -134,7 +215,18 @@ async function handler2(sessionId, messaging) {
 
       return response;
     } else if (answer === "PIROBEBI") {
-      const aboutInfo = "პირობები დამქირავლებლის"; // Replace with actual about information
+      const aboutInfo = `👇 გამარჯობა, გთხოვთ გაეცნოთ ჩვენს პირობებს 👇
+
+      🌹კანდიდატის პირობები🌹 
+      
+      ჩვენ გაგასაუბრებთ თქვენს მოთხოვნებთან მაქსიმალურ შესაბამისობაში 4-5 პასპორტიზირებულ კანდიდატს, ყველასათვის ცნობებით და რომელიმე მოწონებისა და გამოსაცდელი ვადის წარმატებით გავლის შემთხვევაში ჩვენთან იხდით კადრის მოძიების საფასურს, რომელიც განისაზღვრება თვიურ ხელფასს მხოლოდ 10%-ს ერთჯერადად. რაც შედის 1 წლის განმავლობაში, კადრი, თუ წავიდა სამსახურიდან, მისგან დამოუკიდებელი მიზეზების გამო ან თავად დაითხოვეთ, უფასოდ გაგინაცვლებთ 3 ჯერ სხვა კანდიდატურით.
+      
+
+      🌹პერსონალის პირობები🌹 
+      
+      ჩვენი პირობები შემდგომში მდგომარეობს: პირველ თვეს ანაზღაურებიდან ნახევარი ჩამოგეჭრებათ მას შემდეგ, რაც აიღებთ ამ ანაზღაურებას. რეგისტრაცია კი ფასიანია და ავანსად გადახდილი იქნება 30 ლარი.
+      
+      `;
 
       const payload = {
         messaging_type: "RESPONSE",
@@ -143,60 +235,9 @@ async function handler2(sessionId, messaging) {
         },
         message: {
           text: aboutInfo,
+
         },
       };
-
-      const request = await axiosInstance();
-      await deleteSession(sessionId);
-
-      const response = await request.post(
-        `/me/messages?access_token=${access_token}`,
-        payload
-      );
-
-      return response;
-    } else if (answer === "PIROBEBI2") {
-      let locationInfo;
-
-      if (session.lastInteraction === "CONTACT") {
-        locationInfo = "პირობები კანდიდატის"; // Replace with location information related to Contact
-      } else {
-        locationInfo = "პირობები კანდიდატის";
-      }
-
-      const payload = {
-        messaging_type: "RESPONSE",
-        recipient: {
-          id: sessionId,
-        },
-        message: {
-          text: locationInfo,
-        },
-      };
-
-      session.lastInteraction = "LOCATION";
-      await updateSession(sessionId, session);
-
-      const request = await axiosInstance();
-      const response = await request.post(
-        `/me/messages?access_token=${access_token}`,
-        payload
-      );
-
-      return response;
-    } else {
-      const message = `კარგით გისურვებთ წარმატებებს`;
-
-      const payload = {
-        messaging_type: "RESPONSE",
-        recipient: {
-          id: sessionId,
-        },
-        message: {
-          text: message,
-        },
-      };
-
       const request = await axiosInstance();
       await deleteSession(sessionId);
 
@@ -212,479 +253,8 @@ async function handler2(sessionId, messaging) {
     throw error;
   }
 }
-
-// async function handler2(sessionId, messaging) {
-//   const answer = messaging[0]?.postback?.payload;
-//   try {
-//     const session = await getSession(sessionId);
-
-//     if (answer === "CONTACT") {
-//       const contactInfo = "Contact Information"; // Replace with actual contact information
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: contactInfo,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else if (answer === "ABOUT") {
-//       const aboutInfo = "About Us"; // Replace with actual about information
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: aboutInfo,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else if (answer === "LOCATION") {
-//       let locationInfo;
-
-//       if (session.lastInteraction === "CONTACT") {
-//         locationInfo = "Location information related to Contact"; // Replace with location information related to Contact
-//       } else {
-//         locationInfo = generateRandomLocation(); // Replace with your logic to generate a random location
-//       }
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: locationInfo,
-//         },
-//       };
-
-//       session.lastInteraction = "LOCATION";
-//       await updateSession(sessionId, session);
-
-//       const request = await axiosInstance();
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else {
-//       const message = `კარგით გისურვებთ წარმატებებს`;
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: message,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     }
-//   } catch (error) {
-//     console.log("Error acquired in handler 11:", error);
-//     throw error;
-//   }
-// }
-
-
-// Helper function to generate a random location
-
-// async function handler2(sessionId, messaging) {
-//   const answer = messaging[0]?.postback?.payload;
-//   try {
-//     let session = await getSession(sessionId);
-
-//     if (answer === "CONTACT") {
-//       const contactInfo = "Contact Information"; // Replace with actual contact information
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: contactInfo,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else if (answer === "ABOUT") {
-//       const aboutInfo = "About Us"; // Replace with actual about information
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: aboutInfo,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else if (answer === "LOCATION") {
-//       let locationInfo;
-
-//       if (session.lastInteraction === "CONTACT") {
-//         locationInfo = "Location information related to Contact"; // Replace with location information related to Contact
-//       } else {
-//         locationInfo = generateRandomLocation(); // Replace with your logic to generate a random location
-//       }
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: locationInfo,
-//         },
-//       };
-
-//       session.lastInteraction = "LOCATION";
-//       await updateSession(sessionId, session);
-
-//       const request = await axiosInstance();
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     } else {
-//       const message = `კარგით გისურვებთ წარმატებებს`;
-
-//       const payload = {
-//         messaging_type: "RESPONSE",
-//         recipient: {
-//           id: sessionId,
-//         },
-//         message: {
-//           text: message,
-//         },
-//       };
-
-//       const request = await axiosInstance();
-//       await deleteSession(sessionId);
-
-//       const response = await request.post(
-//         `/me/messages?access_token=${access_token}`,
-//         payload
-//       );
-
-//       return response;
-//     }
-//   } catch (error) {
-//     console.log("Error acquired in handler 11:", error);
-//     throw error;
-//   }
-// }
-
-
-
-
-async function handler3(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler4(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler5(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler6(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler7(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler8(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler9(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler10(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler11(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-async function handler12(sessionId) {
-
-  try {
-    const message = "რა ლოკაციაზე ეძებთ პერსონალს?";
-    const payload = {
-      messaging_type: "RESPONSE",
-      recipient: {
-        id: sessionId,
-      },
-      message: {
-        text: message,
-      },
-    };
-    const request = axiosInstance();
-    const response = await request.post(
-      `/me/messages?access_token=${access_token}`,
-      payload
-    );
-    return response;
-  } catch (error) {
-    console.log("error acquired in handler 3: ", error);
-    throw error;
-  }
-}
-
 module.exports = {
   handler1,
   handler2,
   handler3,
-  handler4,
-  handler5,
-  handler6,
-  handler7,
-  handler8,
-  handler9,
-  handler10,
-  handler11,
-  handler12,
 };
